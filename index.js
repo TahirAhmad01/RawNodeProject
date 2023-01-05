629a7544e2341baf4adba7475ef6fd188fe470a6
TahirAhmad01 <tahirahmad0234@gmail.com>
2023-01-05 23:12:40 +0600

    server add

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 
 //dependencies
 const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
 
 //app object - module scaffolding
 const app = {};
@@ -30,9 +32,36 @@ app.createServer = () => {
 };
 
 app.handleReqRes = (req, res) => {
+  // request handling
+
+  //get url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/|\/+$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStringObj = parsedUrl.query;
+  const header = req.headers;
+
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
+
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on("end", () => {
+    realData += decoder.end();
+    console.log(realData);
+    res.end("hello world!");
+  });
+
+  //check if it is a request to /uptime
+  if (parsedUrl.pathname === "/uptime") {
+    res.writeHead(200, {});
+  }
+
   //res handle request
-  res.end("hello world!");
 };
 
 //start server
-app.createServer()
+app.createServer();

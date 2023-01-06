6dcf0adb9a4f8e5bffe04c0b1384e92f9ddfba94
TahirAhmad01 <tahirahmad0234@gmail.com>
2023-01-06 21:15:19 +0600

    cpanel auto deploy done

--- index.js
+++ index.js
@@ -8,8 +8,7 @@
 
 //dependencies
 const http = require("http");
-const url = require("url");
-const { StringDecoder } = require("string_decoder");
+const { handleReqRes } = require("./helpers/handleReqRes");
 
 //app object - module scaffolding
 const app = {};
@@ -31,37 +30,8 @@ app.createServer = () => {
   });
 };
 
-app.handleReqRes = (req, res) => {
-  // request handling
-
-  //get url and parse it
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/|\/+$/g, "");
-  const method = req.method.toLowerCase();
-  const queryStringObj = parsedUrl.query;
-  const header = req.headers;
-
-  const decoder = new StringDecoder("utf-8");
-  let realData = "";
-
-  req.on("data", (buffer) => {
-    realData += decoder.write(buffer);
-  });
-
-  req.on("end", () => {
-    realData += decoder.end();
-    console.log(realData);
-    res.end("hello world!");
-  });
-
-  //check if it is a request to /uptime
-  if (parsedUrl.pathname === "/uptime") {
-    res.writeHead(200, {});
-  }
-
-  //res handle request
-};
+app.handleReqRes = (req, res) => handleReqRes;
 
 //start server
+
 app.createServer();
